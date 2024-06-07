a4f8fc667f397ea4f1a7f5e4ff34de0b23cdcf17
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-06-07 18:58:44 +0530

    Simple Datatpes implementation

---
+++ Datatypes.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main(){
+    int age=21;
+    float petPrice=105.65;
+    double gold=63.012451245;
+    char wing='R';
+
+    printf("%d\n",age);
+    printf("%f\n",petPrice);
+    printf("%lf\n",gold);
+    printf("%c\n",wing);
+}
+//This was my code where I learned about datatypes
