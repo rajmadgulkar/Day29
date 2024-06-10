8053c397462a66e05bc9d8d277f4cb6c64b6d92c
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-06-10 22:20:44 +0530

    Initial Commit

---
+++ Day3/operatots2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int x=20; //initialization of x=20
+int y; //assignment
+//defining an new function
+void office(){
+    printf("Nusta Coding...\n");
+}
+void main(){
+
+    printf("Wakeup\n");
+    office();//calling the function
+    printf("Sleep\n");
+
+
+}
