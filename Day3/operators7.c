8cbd0683ff7213ed2b3e9a06badf2319eb31996a
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-06-10 22:50:09 +0530

    Initail Commit

---
+++ Day3/operators7.c
@@ -0,0 +1,11 @@
+//Code 5
+#include <stdio.h>
+void main(){
+    int a=20;
+    int output;
+    printf("%d\n",a);
+    printf("%d\n",output);
+output=a+10;
+    printf("%d\n",a);
+    printf("%d\n",output);
+}
