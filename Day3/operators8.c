244e6ccd8d07b92970faa51c961f2f6e72abc3a7
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-06-10 22:54:52 +0530

    Initial commit

---
+++ Day3/operators8.c
@@ -0,0 +1,11 @@
+//Code 6 (Pre increment code)
+#include <stdio.h>
+void main(){
+int a=10;
+int output;
+printf("%d\n",a);
+printf("%d\n",output);
+output=++a;
+printf("%d\n",a);
+printf("%d\n",output);
+}
