a0507b265c52138dea25b9d22f135bfbb6899f20
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-06-11 20:28:45 +0530

    Initial Commit

---
+++ Day5/operators1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main(){
+    int x=5;
+    int y=7;
+    int ans;
+    ans=x||y;//logical OR
+    printf("%d\n",ans);
+    ans=x&&y;//logical AND
+    printf("%d\n",ans);
+}
