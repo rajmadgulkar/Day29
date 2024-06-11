845f14eba6ab511a85bb0b81e6e31e94b0c0a262
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-06-11 20:19:00 +0530

    Initial Commit

---
+++ Day5/operators.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main(){
+    int x=30;
+    int y=25;
+    printf("%d\n",x+y);
+    printf("%d\n",x-y);
+    printf("%d\n",x*y);
+    printf("%d\n",x/y);
+    printf("%d\n",x%y);
+}
