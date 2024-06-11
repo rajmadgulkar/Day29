dcf133be6b40ae65a5e22dea4d0e60cabaa4dc32
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-06-11 21:18:26 +0530

    Initial Commit

---
+++ Day5/operators8.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main(){
+    int x=8;
+    int y=7;
+    printf("%d\n",x<=y);
+    printf("%d\n",x==y);
+    printf("%d\n",x>=y);
+    printf("%d\n",x!=y);
+    printf("%d\n",x<y);
+    printf("%d\n",x>y);
+    
+}
