184fb4312eaa1d224caa82377ae17524de86a887
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-06-13 11:55:35 +0530

    Initial commit

---
+++ Day7/ifelse.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main(){
+printf("Start main\n");
+
+int x=0;
+int y=4;
+// Here basically if condition is a boolean value which does not execute the first if block 
+
+if(x){
+printf("In first if block\n");
+}
+if(y){
+printf("In second if block\n");
+}
+
+printf("End main\n");
+}
