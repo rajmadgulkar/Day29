5b4db1e2009a0212dc2848fa613580b7b733551f
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-06-13 12:41:23 +0530

    Initial Commit

---
+++ Day7/ifelse10.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main(){
+
+int x=10;
+char ch1='A';
+char ch2=97;
+char ch3=129;
+
+printf("%d\n",x);
+printf("%c\n",ch1);
+printf("%c\n",ch2);
+printf("%c\n",ch3);// undefined character
+}
