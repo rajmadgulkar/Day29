4f7fb4859475ff50a1596539f54dadedbce2a7f9
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-06-13 12:02:43 +0530

    Initial Commit

---
+++ Day7/ifelse1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main(){
+printf("Start main\n");
+
+char ch1='A';
+char ch2=48;
+
+if (ch1){
+printf("In first if block\n");
+}
+if (ch2){
+printf("In second if block\n");
+}
+
+printf("End main\n");
+
+}
