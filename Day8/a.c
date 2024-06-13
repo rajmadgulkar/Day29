1f63b3790f884eeeef4de431179705d5f6286552
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-06-13 15:17:25 +0530

    initial commit

---
+++ Day8/a.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main(){
+int pMoney;
+printf("Enter the Pocket Money\n");
+scanf("%d",&pMoney);
+
+if(pMoney>=25000){
+printf("Ujjain we are comming\n");
+}else{
+printf("Ghari Basu\n");
+}
+}
