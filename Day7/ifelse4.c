1ee889f6fdc23012d4933ccc0cfdb4fb1de49050
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-06-13 12:22:22 +0530

    Initial Commit

---
+++ Day7/ifelse4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main(){
+
+int x=10;
+char ch1='A';
+char ch2=97;
+//char ch3=128;
+
+printf("%d\n",x);//10
+printf("%c\n",ch1);//A
+printf("%c\n",ch2);//a i.e input character integer but while displaying it is displaying a due to %c.
+//printf("%c\n",ch3);
+}
