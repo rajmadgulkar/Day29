dd8e6bbbd4e1ec4a95a227e98371ffe1c6d34ef8
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-06-15 13:25:49 +0530

    Initial Commit

---
+++ Day10/d.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main(){
+    int strtnum;
+    int endnum;
+
+    printf("Enter the Start number:");
+    scanf("%d",&strtnum);
+    printf("Enter the end number:");
+    scanf("%d",&endnum);
+
+for(int i=strtnum; i<=endnum; i++){
+    printf("Study\n");
+}
+}
+
+// Enter the Start number:2
+// Enter the end number:6
+// Study
+// Study
+// Study
+// Study
+// Study
