48130689a47752763f1ef62202761ac41ef893f0
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-06-15 13:10:15 +0530

    Initial Commit

---
+++ Day10/a.c
@@ -0,0 +1,20 @@
+// 1 1 1 1 1 
+#include <stdio.h>
+void main(){
+
+    printf("1 1 1 1 1");
+
+    printf("\n");
+
+for (int i = 1; i <=5 ; i++)
+{
+    printf("1 ");
+
+}
+
+
+}
+
+
+// 1 1 1 1 1
+// 1 1 1 1 1
