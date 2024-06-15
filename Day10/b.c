e4588273b8012fb5267556d89f01186a40ecf864
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-06-15 13:14:46 +0530

    Initilal Commit

---
+++ Day10/b.c
@@ -0,0 +1,33 @@
+// *
+// *
+// *
+// *
+// *
+#include <stdio.h>
+void main(){
+
+    printf("*\n");
+    printf("*\n");
+    printf("*\n");
+    printf("*\n");
+    printf("*\n");
+
+for (int i = 1; i <=5 ; i++)
+{
+    printf("*\n ");
+
+}
+
+
+}
+
+// *
+// *
+// *
+// *
+// *
+// *
+// *
+// *
+// *
+// *
