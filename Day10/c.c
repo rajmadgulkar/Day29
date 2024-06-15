79ace94e50b6a20eb45dba01a8c2745625f46cce
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-06-15 13:19:40 +0530

    Initial Commit

---
+++ Day10/c.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void main(){
+    int num;
+
+printf("Enter the number:");
+scanf("%d",&num);
+
+for(int i=1; i<=num; i++){
+    printf("Raj\n");
+    
+}
+}
+
+// Enter the number:10
+// Raj
+// Raj
+// Raj
+// Raj
+// Raj
+// Raj
+// Raj
+// Raj
+// Raj
+// Raj
