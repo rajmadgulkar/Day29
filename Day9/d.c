9b14add3a552bad38b3a6b9263d7fd136dacc25e
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-06-15 11:25:16 +0530

    Initial Commit

--- Day9/d.c
+++ Day9/d.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
 void main(){
-        int x;
-        printf("Enter value\n");
-        scanf("%d",&x);
+        int x='A';
+//      printf("Enter value\n");
+//      scanf("%d",&x);
 
         switch(x){
                 case 65:
                         printf("value of x is 65\n");
                         break;
 
-                case 'A':
-                        printf("value of x is A\n");//Duplicate case
-                        break;
                 case 66:
                         printf("value of x is 66\n");
                         break;
-                case 'B':
-                        printf("value of x is B\n");//Duplicate case                                break;
+                case 67:
+                        printf("value of x is 67\n");
+                        break;
+                case 68:
+                        printf("value of x is 68\n");
+                        break;
                 default:
                         printf("wrong input");
                         break;
 
                 }
+}
