64fa2570ce8c82b409d94bdd310cfbca755a1795
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-06-19 22:16:17 +0530

    Initial Commit

---
+++ Day15/a.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void main(){
+        float friendPack1;
+        float friendPack2;
+        float friendPack3;
+        float friendPack4;
+        float friendPack5;
+
+        printf("Enter the packages:\n");
+        scanf("%f",&friendPack1);
+        scanf("%f",&friendPack2);
+        scanf("%f",&friendPack3);
+        scanf("%f",&friendPack4);
+        scanf("%f",&friendPack5);
+
+
+        printf("Friend1 Package: %f\n",friendPack1);
+        printf("Friend1 Package: %f\n",friendPack2);
+        printf("Friend1 Package: %f\n",friendPack3);
+        printf("Friend1 Package: %f\n",friendPack4);
+        printf("Friend1 Package: %f\n",friendPack5);
+
+}
