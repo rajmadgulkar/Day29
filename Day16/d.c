be3a2887d5b721fe7cb1de9f88107030bc650b6c
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-06-20 22:12:37 +0530

    Initial commit

---
+++ Day16/d.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main(){
+        int size;
+        printf("Enter array size:");
+        scanf("%d",&size);
+
+        int arr1[size];
+        int arr2[size];
+
+        printf("Enter array elements:\n");
+        for(int i=0; i<size; i++){
+                scanf("%d",&arr1[i]);
+        }
+
+        for(int i=0; i<size; i++){
+            arr2[i]=arr1[i];
+
+        }
+        for(int i=0; i<size; i++){
+                printf("%d\n",arr2[i]);
+        }
+}
