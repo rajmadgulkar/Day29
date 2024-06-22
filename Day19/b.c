fd3e70aba1c087c6e0934d9a317630b9b2fbf8c1
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-06-22 13:53:44 +0530

    Initial Commit

---
+++ Day19/b.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void main(){
+    int arr[]={10,20,30,40,50};
+
+    int *ptr1=&arr[0];
+    int *ptr2=&arr[1];
+
+    printf("%d\n",*ptr1);
+    printf("%d\n",*ptr2);
+
+
+    int x=ptr1-ptr2;
+    printf("%d\n",x);
+
+
+
+}
+// 10
+// 20
+// -1
