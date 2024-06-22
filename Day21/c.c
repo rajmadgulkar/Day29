c4b244941b5c132bd3475c438900b7c5081a915d
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-06-22 15:23:12 +0530

    Initial Commit

---
+++ Day21/c.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main(){
+    int arr[]={10,20,30,40,50};
+    int *ptr=&arr[0];
+    printf("%d\n",*ptr);
+    (*ptr)++;
+    for(int i=0; i<5;i++){
+        printf("%d\n",arr[i]);
+
+    }
+}
