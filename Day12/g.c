a019025ca3def1a45153c0aeb4e1381290094e45
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-06-28 17:20:28 +0530

    Initial Commit

---
+++ Day12/g.c
@@ -0,0 +1,21 @@
+/* 
+a c e
+g i k
+m o q
+*/
+
+#include <stdio.h>
+void main(){
+    char ch='a';
+
+    for (int i=1; i<=3;i++){
+
+        for(int j=1; j<=3;j++){
+            printf(" %c",ch);
+            ch=ch+2;
+        }
+        printf("\n");
+        
+
+    }
+}
