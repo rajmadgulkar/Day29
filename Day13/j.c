133344d90a34f7cdb0d719cff157bb6156a52763
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-06-28 18:05:01 +0530

    Initial Commit

---
+++ Day13/j.c
@@ -0,0 +1,23 @@
+/* 
+ 1
+ 4 9
+ 16 25 36
+ 49 64 81 100
+ 121 144 169 196 225
+*/
+
+#include <stdio.h>
+void main(){
+    int x=1;
+
+    for (int i=1; i<=5;i++){
+
+        for(int j=1; j<=i;j++){
+            printf(" %d",x*x);
+            x++;
+        }
+        printf("\n");
+        
+
+    }
+}
