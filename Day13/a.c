1069251c5f3279ce016ec4f8f51b4ebe5733ef20
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-06-28 17:48:35 +0530

    Initial commit

---
+++ Day13/a.c
@@ -0,0 +1,21 @@
+ /*
+ *
+ * *
+ * * *
+ * * * *
+*/
+
+
+
+
+#include <stdio.h>
+void main(){
+    for(int i=1; i<=4;i++){
+        for (int j=1; j<=i;j++){
+            printf(" *");
+
+        }
+        printf("\n");
+    }
+             
+}
---
+++ Day13/b.c
@@ -0,0 +1,22 @@
+ /*
+ 1
+ 1 1
+ 1 1 1
+ 1 1 1 1
+*/
+
+
+
+
+#include <stdio.h>
+void main(){
+    for(int i=1; i<=4;i++){
+        int x=1;
+        for (int j=1; j<=i;j++){
+            printf(" %d",x);
+
+        }
+        printf("\n");
+    }
+             
+}
