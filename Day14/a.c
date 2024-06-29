84d2df7bf431cc54913438927c8925da9dc4630a
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-06-29 17:15:03 +0530

    initial Commit

---
+++ Day14/a.c
@@ -0,0 +1,16 @@
+/* 
+ * * * * *
+ * * * *
+ * * *
+ * *
+ *
+*/
+#include<stdio.h>
+void main(){
+    for(int i=1; i<=5;i++){
+        for(int j=5;j>=i;j--){
+            printf(" *");
+        }
+        printf("\n");
+    }
+}
