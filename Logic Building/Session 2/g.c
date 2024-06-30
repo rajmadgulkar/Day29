1986abc6f6667d34341b888c4b36078c56740908
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-06-30 18:16:37 +0530

    Initial Commit

--- Logic Building/Session 2/g.c
+++ Logic Building/Session 2/g.c
@@ -1,11 +1,26 @@
 #include<stdio.h>
 void main(){
     int rows;
-    int x=1;
+    
     printf("ROWS:");
     scanf("%d",&rows);
 
-    for(int i=rows; i>=1;i--{
-        for(int j=)
-    })
+    char ch='D';
+
+    for(int i=rows; i>=1;i--){
+        for(int j=1;j<=rows;j++){
+            printf(" %c",ch);
+
+
+        }
+        ch--;
+        printf("\n");
+    }
 }
+/*
+ROWS:4
+ D D D D
+ C C C C
+ B B B B
+ A A A A
+ */
