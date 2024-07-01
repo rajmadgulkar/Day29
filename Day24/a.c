62641f1b80fe66ea15691170355e6533b6a373d8
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-07-01 15:16:29 +0530

    Initial Commit

---
+++ Day24/a.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main(){
+    int arr[3]={1,2,3,4};
+    int arr1[][3]={{1,2,3,4},{5,6},{7,8,9}};
+
+    printf("%d\n",arr[3]);
+    printf("%d\n",arr1[0][3]);
+}
+/*
+1
+5
+*/
