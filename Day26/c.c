8ffc22a7d69f3e7a28103efce960779b07376193
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-07-01 20:19:26 +0530

    Initial Commit

---
+++ Day26/c.c
@@ -0,0 +1,19 @@
+// Pointer  to an array
+#include<stdio.h>
+void main(){
+    int arr[5]={10,20,30,40,50};
+    int *ptr1=arr;
+    int *ptr2=&arr[2];
+    int (*ptr3)[5]=&arr;
+
+    printf("%d\n",*ptr1);
+    printf("%d\n",*ptr2);
+    printf("%p\n",*ptr3);
+    printf("%d\n",**ptr3);
+}
+/*
+10
+30
+000000c51ebff710
+10
+*/
