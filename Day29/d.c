133f4115763874415535a012cd727b8a27ec2373
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-07-04 18:57:56 +0530

    Initial Commit

---
+++ Day29/d.c
@@ -0,0 +1,37 @@
+
+//Call by value
+#include<stdio.h>
+void swap(int, int);
+void main(){
+    int x=10;
+    int y=20;
+    printf("x= %d\n",x);
+    printf("y= %d\n",y);
+
+    swap(x,y);
+
+    printf("x= %d\n",x);
+    printf("y= %d\n",y);   
+}
+void swap(int x,int y){
+    
+    printf("x= %d\n",x);
+    printf("y= %d\n",y);
+    
+    int temp;
+    temp=x;
+    x=y;
+    y=temp;
+    printf("x= %d\n",x);
+    printf("y= %d\n",y);
+}
+/*
+x= 10
+y= 20
+x= 10
+y= 20
+x= 20
+y= 10
+x= 10
+y= 20
+*/
