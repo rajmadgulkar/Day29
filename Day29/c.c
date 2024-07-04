97434aab0f500c08226a42c63f2ca8765e68b997
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-07-04 17:53:57 +0530

    Initial Commit

---
+++ Day29/c.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void AddofNum(int ,int ,int );
+
+void main(){
+    int x=10;
+    int y=20;
+    int z=30;
+    
+    AddofNum(x,y,z);
+
+
+}
+void AddofNum(int x,int y, int z){
+    printf("Add=%d\n",x+y+z);
+
+}
